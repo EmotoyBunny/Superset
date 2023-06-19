e6d19c7180724d43b05936fbb46653201436c87e
Яков Кустря <kustrya-yaya@rshbext.ru>
2023-06-19 16:43:58 +0300

    Создал отдельный проект для сервиса получения guest-token

--- chart-test/src/App.jsx
+++ chart-test/src/App.jsx
@@ -1,96 +1,110 @@
-import './App.css';
-import React from "react";
-/*import {ChartMain} from "./components/ChartMain";
-import {MainMenu} from "./components/MainMenu";
-import {Grid} from "@mui/material"*/
-import {embedDashboard} from "@superset-ui/embedded-sdk";
+import { embedDashboard } from "@superset-ui/embedded-sdk"
 import { useEffect } from "react"
+import "./App.css"
 
 function App() {
-    const getToken = async () => {
-        const response = await fetch("/guest-token")
-        console.log(response)
-        const token = await response.json()
-        return token
+
+  async function fetchAccessToken() {
+    try {
+      const body = {
+        username: "UserForGuestToken",
+        password: "Qge-vhr-Z6T-G3B",
+        provider: "db",
+        refresh: true,
+      }
+
+      const response = await fetch(
+        "http://superset.kopr:8088/api/v1/security/login",
+        {
+          method: "POST",
+          body: JSON.stringify(body),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+
+      const jsonResponse = await response.json()
+
+      console.log(jsonResponse)
+
+      return jsonResponse?.access_token
+    } catch (error) {
+      console.error(error)
     }
+  }
 
-    useEffect(() => {
-        const embed = async () => {
-            await embedDashboard({
-                id: "45c5846b-aa8c-47c7-9d61-3d4559efc1f1",
-                supersetDomain: "http://172.18.5.243:8088",
-                mountPoint: document.getElementById("dashboard"), // html element in which iframe render
-                fetchGuestToken: () => getToken(),
-                dashboardUiConfig: {
-                    hideTitle: true,
-                    hideChartControls: true,
-                    hideTab: true,
-                },
-            })
+  async function fetchGuestToken() {
+    const accessToken = await fetchAccessToken()
+    try {
+      const body = {
+        resources: [
+          {
+            type: "dashboard",
+            id: "b3ff142c-67a8-4318-82c1-4a6ae769f565",
+          },
+        ],
+        rls: [],
+        user: {
+          username: "UserForDashboard",
+          first_name: "NameUserForDashboard",
+          last_name: "FamilyUserForDashboard",
+        },
+      }
+      const response = await fetch(
+        "http://superset.kopr:8088/api/v1/security/guest_token",
+        {
+          method: "POST",
+          body: JSON.stringify(body),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
         }
-        if (document.getElementById("dashboard")) {
-            embed();
-        }
-    }, [])
+      )
+      const jsonResponse = await response.json()
 
-    return (
-        <div className="App">
-            <h1>How to Embed Superset Dashboard into React</h1>
-            <div id="dashboard"/>
-        </div>
-    )
+      console.log(jsonResponse)
+
+      return jsonResponse?.token
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const getToken = async () => {
+    const response = await fetch("http://localhost:5000/guest-token")
+    const token = await response.json()
+    return token
+    // return "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7Imxhc3RfbmFtZSI6IkZhbWlseVVzZXJGb3JEYXNoYm9hcmQiLCJ1c2VybmFtZSI6IlVzZXJGb3JEYXNoYm9hcmQiLCJmaXJzdF9uYW1lIjoiTmFtZVVzZXJGb3JEYXNoYm9hcmQifSwicmVzb3VyY2VzIjpbeyJpZCI6ImIzZmYxNDJjLTY3YTgtNDMxOC04MmMxLTRhNmFlNzY5ZjU2NSIsInR5cGUiOiJkYXNoYm9hcmQifV0sInJsc19ydWxlcyI6W10sImlhdCI6MTY4NjkyNDE4MS4xMTA0NjksImV4cCI6MTY4NjkyNDQ4MS4xMTA0NjksImF1ZCI6Imh0dHA6Ly9zdXBlcnNldDo4MDg4LyIsInR5cGUiOiJndWVzdCJ9.VON5JbtBtAHLM3U568h4AGQxp0SDtzbFuLYOg3zOvOA"
+  }
+
+  useEffect(() => {
+    const embed = async () => {
+      await embedDashboard({
+        id: "b3ff142c-67a8-4318-82c1-4a6ae769f565", // given by the Superset embedding UI
+        supersetDomain: "http://superset.kopr:8088",
+        mountPoint: document.getElementById("dashboard"), // html element in which iframe render
+        fetchGuestToken: () => getToken(),
+//        fetchGuestToken: () => fetchGuestToken(),
+        dashboardUiConfig: {
+          hideTitle: true,
+          hideChartControls: true,
+          hideTab: true,
+        },
+      })
+    }
+    if (document.getElementById("dashboard")) {
+      embed()
+    }
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>How to Embed Superset Dashboard into React</h1>
+      <div id="dashboard" />
+    </div>
+  )
 }
 
-/*class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    render() {
-        return (
-            <div
-                style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "flex-end",
-                    padding: "0 8px",
-                }}
-            >
-                <MainMenu
-                    appName=""
-                    components={{
-                        ToolbarButtons: (props) => {
-                            return (
-                                <Grid container style={{width: 50}}>
-                                </Grid>
-                            );
-                        },
-
-                        Menu: (props) => {
-                            return (
-                                <div>
-                                </div>
-                            );
-                        },
-                    }}
-                />
-                <div style={{marginTop: "70px", height: "600px", width: "100%", flexGrow: 1}} >
-                    <iframe
-                        title="Dashboard"
-                        src="http://localhost:8088/login?token=1234abcd456&next=/superset/dashboard/11?standalone=3"
-                        width="100%"
-                        height="800px"
-                        sandbox="allow-same-origin allow-scripts"
-                        id={"superset-container"}
-                    >
-                    </iframe>
-                    {/!*<div >
-                    </div>*!/}
-                </div>
-            </div>
-        );
-    }
-}*/
-
-export default App;
+export default App
